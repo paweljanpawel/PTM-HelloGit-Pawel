468c8cf2867b3ed187a51c5cbecf9e394e79cb5b
pawel <pa.januzik@gmail.com>
2018-03-18 19:05:35 +0100

    Initial commit

---
+++ PTM-HelloGit-Pawel.cpp
@@ -0,0 +1,13 @@
+// PTM-HelloGit-Pawel.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello Git"<<std::endl;
+    return 0;
+}
+
